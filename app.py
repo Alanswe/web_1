969cb17bbaa7c0ff44099b9caf17afd3fcfbe572
Alanswe <92720019+Alanswe@users.noreply.github.com>
2022-03-25 11:21:11 +0100

    main

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import os
-from bottle import route,run, TEMPLATE_PATH,jinja2_view,static_file
+from bottle import route,run, TEMPLATE_PATH,jinja2_view,static_file,request
+import sqlite3
 
 TEMPLATE_PATH.append(os.path.join(os.path.dirname(__file__),'templates'))
+
+BASE_DATOS = os.path.join(os.path.dirname(__file__),'personas.db') # en sqlite, una base de datos es un fichero
+
 
 @route('/static/<filename>')
 def server_static(filename):
@@ -17,5 +21,44 @@ def hola():
         ]
         }
     #return {'Nombre': 'Alan','Fecha': '23/09/2022'}
+
+@route('/formulario')
+@jinja2_view('formulario.html')
+def mi_form():
+    return {}
+
+@route('/guardar',method='POST')
+def guardar():
+    # checoche = []
+    # nombre = request.POST.usuario
+    # correo = request.POST.correo
+    # clave1 = request.POST.clave1
+    # cars = request.POST.cars
+    # num_ratio = request.POST.num_ratio
+    # if 'vehiculo' in request.POST:
+    #     vehiculos = request.POST.dict['vehiculo']
+
+    # if 'vehiculo' in request.POST:
+    #     vehiculos = request.POST.getlist('vehiculo')
+
+    # vehiculo = list(request.POST.vehiculo)
+
+    # if 'checoches1' in request.POST:
+    #     checoche.append(request.POST.checoches1)
+    # if 'checoches2' in request.POST:
+    #     checoche.append(request.POST.checoches2)
+    # if 'checoches3' in request.POST:
+    #     checoche.append(request.POST.checoches3)
+
+    # ----------------------------------- formualrio 2
+    nombre = request.POST.nombre
+    apellidos = request.POST.apellidos
+    dni = request.POST.dni
+
+    cnx = sqlite3.connect(BASE_DATOS)
+    consulta = "INSERT into persona (nombre,apellidos,dni) values(?,?,?)"
+    cnx.execute(consulta,(nombre,apellidos,dni))
+    cnx.commit()#es para guardar
+    cnx.close()#cerrar siempre la consulta
 
 run(host='localhost',port=8080,debug=True)
